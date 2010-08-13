1b2bbe9fa937f48cd77318f3d8c54a7a020f2fe9
Cody Brocious <cody.brocious@gmail.com>
2010-08-13 05:26:07 -0400

    Added logging and lots of it.

--- ventroulette/controllers/Chat.py
+++ ventroulette/controllers/Chat.py
@@ -53,6 +53,7 @@ class ChatController(BaseController):
 	def getChatId(self):
 		global curId
 		type = request.params.get('type', None)
+		log.info('[getChatId] Getting chat ID for type %r' % type)
 		if type == u'listener':
 			type = 0
 		elif type == u'venter':
@@ -71,14 +72,18 @@ class ChatController(BaseController):
 			else:
 				ret = request.environ['cogen.wsgi'].result
 				if ret != None:
+					log.info('[getChatId] Found queue, %r' % ret)
 					# Got a user.  Check to make sure the queue is still there and not taken.
 					if not ret in queues or queues[ret][type][1].started:
+						log.info('[getChatId] Queue %r nonexistent or started.' % ret)
 						continue
 					# Check to make sure the queue hasn't died.  Delete it if it has.
 					if queues[ret][type ^ 1][1].isDead():
+						log.info('[getChatId] Queue %r is dead.' % ret)
 						del queues[ret]
 						continue
 					# Start new user's dead timer, push True onto both sides of the queue to start.
+					log.info('[getChatId] Queue %r is good.' % ret)
 					queues[ret][type][1].started = True
 					yield request.environ['cogen.call'](queues[ret][type][0].put)(True)
 					yield request.environ['cogen.call'](queues[ret][type ^ 1][0].put)(True)
@@ -90,6 +95,7 @@ class ChatController(BaseController):
 		# No waiting user found.  Create a new queue.
 		id = curId
 		curId += 1
+		log.info('[getChatId] Adding new queue, %r' % id)
 		queues[id] = ((queue.Queue(), DeadTimer()), (queue.Queue(), DeadTimer()))
 		# Start dead timer.
 		queues[id][type][1].started = True
@@ -110,14 +116,17 @@ class ChatController(BaseController):
 		
 		# Split up chat ID
 		a, b = chatId >> 1, (chatId & 1) ^ 1
+		log.info('[send] Sending message %r to (%r, %r)' % (msg, a, b))
 		# If queue is missing, return false.
 		if not a in queues:
+			log.info('[send] (%r, %r) is missing' % (a, b))
 			yield 'false'
 			return
 		
 		# Escape message and add it to the queue.
 		yield request.environ['cogen.call'](queues[a][b][0].put)(cgi.escape(msg))
 		val = request.environ['cogen.wsgi'].result
+		log.info('[send] Message sent to (%r, %r)' % (a, b))
 		yield 'true'
 	
 	def recv(self):
@@ -129,8 +138,10 @@ class ChatController(BaseController):
 		
 		# Split up chat ID
 		a, b = chatId >> 1, chatId & 1
+		log.info('[recv] Receiving message from (%r, %r)' % (a, b))
 		# If queue is missing, return false.
 		if not a in queues:
+			log.info('[recv] (%r, %r) is missing' % (a, b))
 			yield 'false'
 			return
 		
@@ -138,6 +149,7 @@ class ChatController(BaseController):
 		queues[a][b][1].update()
 		# Check other user's dead timer, killing queue and returning false if dead.
 		if queues[a][b^1][1].isDead():
+			log.info('[recv] (%r, %r) is dead' % (a, b))
 			del queues[a]
 			yield 'false'
 			return
@@ -153,7 +165,10 @@ class ChatController(BaseController):
 			if ret == None:
 				yield 'null'
 			else:
+				log.info('[recv] Received %r from (%r, %r)' % (ret, a, b))
 				yield json.dumps(ret)
+				return
+		log.info('[recv] Received nothing from (%r, %r)' % (a, b))
 	
 	def newPartner(self):
 		try:
@@ -164,10 +179,11 @@ class ChatController(BaseController):
 		
 		# Split up chat ID
 		a, b = chatId >> 1, (chatId & 1) ^ 1
+		log.info('[newPartner] Getting new partner for (%r, %r)' % (a, b))
 		# If queue exists, put False in it (to save a round trip, if the user is 
 		# waiting for a message) and delete it.
 		if a in queues:
+			log.info('[newPartner] (%r, %r) still existed.  Killing.' % (a, b))
 			yield request.environ['cogen.call'](queues[a][b][0].put)(False)
-			val = request.environ['cogen.wsgi'].result
 			del queues[a]
 		yield 'true'
